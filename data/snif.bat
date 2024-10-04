193c85a1c86bee124c07845ae4885f69320bec49
Duke Manchester <73570126+dukeman74@users.noreply.github.com>
2024-10-03 18:26:21 -0600

    added both ip's I've seen

--- data/snif.bat
+++ data/snif.bat
@@ -1,2 +1,2 @@
-C:\"Program Files\Wireshark\tshark.exe" -i ethernet -f "src host 35.71.175.214" -w data\packets -c 3
+C:\"Program Files\Wireshark\tshark.exe" -i ethernet -f "src host 52.223.44.23 or src host 35.71.175.214" -w data\packets -c 3
 #timeout /t 30
